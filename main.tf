665665847fccefa9644e7a91e689d0de0442d2fb
Tucky95 <110927459+Tucky95@users.noreply.github.com>
2025-02-18 16:27:34 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "<your subscription id here>"
+  subscription_id = "10be9956-8975-43fc-80a4-61ff8b002e90"
 }
 
 terraform {
   backend "azurerm" {
     resource_group_name  = "RG1"
-    storage_account_name = "<your storage account>"
+    storage_account_name = "jenkinsstatejames"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
